614d386dfb0e70388ab1462163f0ad67f2827865
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-06 13:58:04 +0000

    Create env file to hide sensitive data

--- bessie_beau/settings.py
+++ bessie_beau/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+if os.path.isfile('env.py'):
+    import env #noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x$9o!iif^fpik@iz+!gl^t^&z#oye+phgi1s07vo1t6mln0w*x'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
