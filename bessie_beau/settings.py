0ea72f9d5c0cc2e36f3eeb811ab1a61afeae4523
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-12 18:40:35 +0000

    Add Heroku app hostname to allowed hosts in settings.py

--- bessie_beau/settings.py
+++ bessie_beau/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bessiebeau.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
