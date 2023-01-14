62a00edbdc3bf348994d15f4dfa1961c669ce1df
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-14 19:03:14 +0000

    Fix code alignments

--- bessie_beau/settings.py
+++ bessie_beau/settings.py
@@ -60,19 +60,19 @@ ROOT_URLCONF = 'bessie_beau.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [
+                os.path.join(BASE_DIR, 'templates')
             ],
-        },
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
     },
 ]
 
