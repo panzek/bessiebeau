32ce3b6f1eb333748645b10333dfd1220a9132a8
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-17 11:31:52 +0000

    Add redirection urls for login and logout

--- bessie_beau/settings.py
+++ bessie_beau/settings.py
@@ -63,6 +63,10 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
