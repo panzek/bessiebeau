156e98945fe2bda18ad5e1033385e807051ea9f6
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-19 18:58:36 +0000

    Connect a receiver to a signal sent by specific users

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from django_countries.fields import CountryField
 
 
@@ -16,3 +19,16 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update user profile
+    """
+    print("sender -->", sender)
+    print("instance -->", instance)
+    print("created -->", created)
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
