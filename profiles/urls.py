fb8e45df4fbff09bb6d134f4cddd85d9dd5ebcac
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-23 14:41:49 +0000

    Add delete profile functionality

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('edit/', views.edit_profile, name='edit_profile'),
+    path('edit/<str:id>', views.edit_profile, name='edit_profile'),
+    path('delete/<str:id>/', views.delete_profile, name='delete_profile'),
 ] 
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile
+from .models import UserProfile, User
 from .forms import UserProfileForm
 
 
@@ -16,23 +16,35 @@ def profile(request):
 
 
 @login_required(login_url='/accounts/login/')
-def edit_profile(request):
+def edit_profile(request, *args, **kwargs):
     """ A view to render edit user Profile page """
+    userprofile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
         form = UserProfileForm(
                 request.POST, 
                 request.FILES, 
-                instance=request.user.userprofile
+                instance=userprofile
             )
         if form.is_valid:
             form.save()
             return redirect('/profile/')
     else:
-        form = UserProfileForm(instance=request.user.userprofile)
+        form = UserProfileForm(instance=userprofile)
     
     context = {
         'form': form
     }
 
     return render(request, 'profiles/edit_profile.html', context)
+
+
+def delete_profile(request, *args, **kwargs):
+    """ A view to render delete user Profile """
+    
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    # u = User.objects.get(id=1)
+    # u = User.objects.filter(username=userprofile) 
+    userprofile.delete()
+
+    return render(request, '')
