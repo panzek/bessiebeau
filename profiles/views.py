50eb025b385a17262cfebc27b6a08facff834459
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-23 00:41:38 +0000

    Add login required decorator

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 
+@login_required(login_url='/accounts/login/')
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
 
@@ -13,6 +15,7 @@ def profile(request):
     return render(request, 'profiles/profile.html', context)
 
 
+@login_required(login_url='/accounts/login/')
 def edit_profile(request):
     """ A view to render edit user Profile page """
 
