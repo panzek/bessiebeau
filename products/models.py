10da90240798361a37d0c412f61b282bcb255825
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-24 21:56:14 +0000

    Create category model and register in admin

--- products/models.py
+++ products/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=250)
+    friendly_name = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
     """ 
     Product model
