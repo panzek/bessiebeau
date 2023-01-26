02c4fb64d4489094e06e7a4b908f94eb71948188
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-26 11:48:59 +0000

    Add views and urls for store owner to add products

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
             views.product_detail, 
             name='product_detail'
         )
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -72,3 +73,28 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    """ A view for store owner to add products to the store """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can add products')
+        return redirect(reverse('home'))
+    
+    if request.method == 'POST':
+        form = ProductForm(request.Post, request.Files)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid')
+    else:
+        form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form
+    }
+
+    return render(request, template, context)
