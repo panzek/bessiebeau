7923cc9170bda9767fa7376006183d61bff4376f
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-26 02:19:08 +0000

    Implement sort selector functionality

--- products/views.py
+++ products/views.py
@@ -32,7 +32,7 @@ def all_products(request):
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-                products = products.order_by(sortkey)
+            products = products.order_by(sortkey)
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -53,7 +53,8 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_sorting': current_sorting
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
