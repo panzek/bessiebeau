70866b33df777515e72018edce9eb515cc93e643
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-27 14:01:27 +0000

    Create add to cart view and its urls

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
     """ A view to render the cart contents page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ A view to add items to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+    
+    request.session['cart'] = cart
+
+    return redirect(redirect_url)
