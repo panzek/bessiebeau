b8d25422b13a76a08913ab0b0da04d07f77c7f22
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-27 18:58:49 +0000

    Add products to the shopping cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
-from decimal import Decimal
+from django.shortcuts import get_object_or_404
 from django.conf import settings
+from decimal import Decimal
+from products.models import Product
 
 
 def cart_contents(request):
@@ -7,6 +9,17 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -24,7 +37,7 @@ def cart_contents(request):
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total
+        'grand_total': grand_total,
     }
 
     return context
