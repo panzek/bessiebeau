6b9b40801a20328d990e4ee660786b7c174de6e7
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-01-30 00:50:43 +0000

    Add Order and OrderLineItems to admin

--- checkout/models.py
+++ checkout/models.py
@@ -6,7 +6,8 @@ from django.conf import settings
 
 from django_countries.fields import CountryField
 
-from profiles.models import UserProfile, Product
+from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
