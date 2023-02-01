3e3bded4fb8f3b4e38324977ff932e014a72ca67
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-02-01 17:28:40 +0000

    Add contact views and urls

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact_us(request):
+    """
+    A view to render form in template
+    """
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form = form.save()
+
+            messages.success(request, 'Contact form was successfully submitted')
+            return redirect('')
+        else:
+            messages.error(request, 'Failed to submit contact form. Please ensure the form is valid')
+    else:
+        form = ContactForm()
+
+        context = {
+            'form': form,
+        }
+
+    return render(request, 'contact/contact.html', context)
+
