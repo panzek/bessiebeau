5e682485779f0b363b7eec83ab0c96d3b66cf6a1
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-02-02 16:41:19 +0000

    Add views, urls and template to edit testimonials

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('add_testimonial/', views.add_testimonial, name='testimonial'),
+    path('edit_testimonial/<int:testimonial_id>', views.edit_testimonial, name='edit_testimonial'),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.contrib import messages
 
 from .forms import TestimonialForm
 
-from profiles.models import UserProfile
 from testimonials.models import Testimonial
 
 
@@ -24,3 +22,23 @@ def add_testimonial(request):
     }
 
     return render(request, 'testimonials/add_testimonial.html', context)
+
+
+def edit_testimonial(request, testimonial_id):
+    """ A view to to edit customer's testimonial """
+
+    testimonial = Testimonial.objects.get(pk=testimonial_id)
+    if request.method == 'POST':
+
+        form = TestimonialForm(request.POST, instance=testimonial)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('home'))
+    
+    form = TestimonialForm(instance=testimonial)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'testimonials/edit_testimonial.html', context)
