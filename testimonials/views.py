32079ffc0a6741252fb4916c25a33c8f952c04de
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-02-02 16:56:45 +0000

    Add views and urls to delete testimonial

--- testimonials/views.py
+++ testimonials/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .forms import TestimonialForm
 
 from testimonials.models import Testimonial
+from profiles.models import UserProfile
 
 
 def add_testimonial(request):
@@ -27,9 +28,13 @@ def add_testimonial(request):
 def edit_testimonial(request, testimonial_id):
     """ A view to to edit customer's testimonial """
 
-    testimonial = Testimonial.objects.get(pk=testimonial_id)
-    if request.method == 'POST':
+    testimonial = get_object_or_404(Testimonial, pk=testimonial_id)
+    user = get_object_or_404(UserProfile, user=request.user)
 
+    if not request.user != testimonial.user:
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
         form = TestimonialForm(request.POST, instance=testimonial)
         if form.is_valid():
             form.save()
@@ -39,6 +44,19 @@ def edit_testimonial(request, testimonial_id):
 
     context = {
         'form': form,
+        'user_profile': user,
     }
 
     return render(request, 'testimonials/edit_testimonial.html', context)
+
+
+def delete_testimonial(request, testimonial_id):
+    """ A view to delete customer's testimonial """
+
+    testimonial = get_object_or_404(Testimonial, pk=testimonial_id)
+
+    if not request.user != testimonial.user:
+        return redirect(reverse('home'))
+
+    testimonial.delete()
+    return redirect(reverse('home'))
