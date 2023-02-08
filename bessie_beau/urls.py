dd89900bb9b86b625005ec0a633b2502597afba6
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2023-02-08 01:59:44 +0000

    Error handling - add 404 view and url

--- bessie_beau/urls.py
+++ bessie_beau/urls.py
@@ -29,3 +29,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('testimonial/', include('testimonials.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = "bessie_beau.views.handle_404"
